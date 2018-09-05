07d4663f4b9ec43a117d20a5d7a8f8f91d57d2e7
Julian Beck <ju-fa-beck@t-online.de>
2018-09-05 18:57:09 +0900

    new electron-vue project

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="wrapper">
+    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
+    <main>
+      <div class="file">
+        <label class="file-label">
+          <input class="file-input" multiple type="file" name="resume" @change="handleFileChange">
+          <span class="file-cta">
+            <span class="file-icon">
+              <i class="fas fa-upload"></i>
+            </span>
+            <span class="file-label">
+              Choose a file(s)…
+            </span>
+          </span>
+        </label>
+      </div>
+      <div class="file">
+        <label class="file-label">
+          <input class="file-input" type="file" name="resume" webkitdirectory directory @change="handleFileChange">
+          <span class="file-cta">
+            <span class="file-icon">
+              <i class="fas fa-upload"></i>
+            </span>
+            <span class="file-label">
+              Choose a folder…
+            </span>
+          </span>
+        </label>
+      </div>
+      <div><file-dropdown></file-dropdown></div>
+      
+    </main>
+  </div>
+</template>
+
+<script>
+  import FileDropdown from './LandingPage/FileDropdown'
+  import vito from '@/services/videos'
+
+  export default {
+    name: 'landing-page',
+    components: {
+      FileDropdown
+
+    },
+    methods: {
+      handleFileChange(e) {
+        // Whenever the file changes, emit the 'input' event with the file data.
+        vito.getFiles(e.target.files);
+      }
+    }
+  }
+</script>
+
+<style>
+  
+</style>
---
+++ src/renderer/components/LandingPage/FileDropdown.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <div id="dropzone">
+      <div>put files here</div>
+      <input type="file" accept="image/png, application/pdf" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+     mounted () {
+      this.$nextTick(() => {
+        this.$el.addEventListener('drop', this.onDrop)
+        this.$el.addEventListener('dragover', this.onDragover)
+        this.$el.addEventListener('dragleave', this.onDragleave)
+      })
+    },
+    beforeDestroy () {
+      this.$el.removeEventListener('drop', this.onDrop)
+      this.$el.removeEventListener('dragover', this.onDragover)
+      this.$el.removeEventListener('dragleave', this.onDragleave)
+    },
+    methods: {
+      onDrop(e) {
+        const files = e.dataTransfer.files
+        const videos = []
+        console.log(files)
+      },
+      onDragover() {
+        this.dragging = true
+      },
+      onDragleave() {
+        this.dragging = false
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  body {
+    background: #333;
+  }
+
+  #dropzone {
+    position: relative;
+    border: 10px dotted black;
+    border-radius: 20px;
+    color: black;
+    font: bold 24px/200px arial;
+    height: 200px;
+    margin: 30px auto;
+    text-align: center;
+    width: 200px;
+  }
+
+  #dropzone.hover {
+    border: 10px solid #FE5;
+    color: #FE5;
+  }
+
+  #dropzone.dropped {
+    background: #222;
+    border: 10px solid #444;
+  }
+
+  #dropzone div {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+
+  #dropzone img {
+    border-radius: 10px;
+    vertical-align: middle;
+    max-width: 95%;
+    max-height: 95%;
+  }
+
+  #dropzone [type="file"] {
+    cursor: pointer;
+    position: absolute;
+    opacity: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+</style>
