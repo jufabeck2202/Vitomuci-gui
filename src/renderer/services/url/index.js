c4766bfae1671d832513e94781fa2f4c183c4047
Julian Beck <ju-fa-beck@t-online.de>
2018-09-06 16:16:07 +0900

    fetch youtube videos and rss feed url

--- src/renderer/services/url/index.js
+++ src/renderer/services/url/index.js
@@ -2,6 +2,7 @@ const isUrl = require("is-url");
 const ytlist = require("youtube-playlist");
 const parsePodcast = require('node-podcast-parser');
 const path = require("upath");
+const request = require('request');
 
 
 /**
@@ -27,49 +28,58 @@ function getRSS(url) {
 
 
 /**
- * Returns array of links if url is a playlist
+ * Returns array of episodes, title and url
  * @param {String} url of the youtube video 
  */
 async function getPlaylist(url) {
     return new Promise((resolve, reject) => {
-        ytlist(url, "url").then(res => {
-            resolve(res.data.playlist);
+        ytlist(url, "url").then(urls => {
+            ytlist(url, "name").then(title => {
+                let episodes = [];
+                for (let i = 0; i < urls.data.playlist.length; i++) {
+                    episodes.push({
+                        title: title.data.playlist[i],
+                        url: urls.data.playlist[i]
+                    });
+                }
+                resolve(episodes);
+            });
         });
     });
 }
 
-
-async function getContent(url){
-
+/**
+ * Returns list of podcast or youtube videos
+ * @param {*} url url to podcast or rss feed
+ */
+async function getContent(url) {
     //Download yt videos
     if (isUrl(url)) {
+        let episodes = [];
         if (url.indexOf("https://www.youtube.com/") >= 0) {
-            //run get playlist
-            let videos
 
             //check if single video or playlist
             try {
-                videos = await getPlaylist(url);
-                if (videos.length == 0) videos = [url];
+                episodes = await getPlaylist(url);
             } catch (error) {
-                console.log(error);
-                videos = [url];
             }
-            return videos;
+            return episodes;
         } else {
             //check for podcast
-            let rss
+            let rss;
             try {
                 rss = await getRSS(url);
+                rss.episodes.forEach(episode => {
+                    episodes.push({title:episode.title,url:episode.enclosure.url})
+                });
+                return episodes;
             } catch (error) {
                 throw (url + " is not a YouTube or RSS feed url");
             }
-            return rss
-            //create podcast output folder
         }
 
     }
 }
 export default {
     getContent
-  }
+}
