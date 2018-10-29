137896f0b98fb08558169b8e0507197deb98610e
Julian Beck <ju-fa-beck@t-online.de>
2018-10-29 07:44:34 +0100

    Fixed selection list

--- src/renderer/components/Download.vue
+++ src/renderer/components/Download.vue
@@ -2,15 +2,14 @@
   <div id="wrapper">
     <div class="overflow">
       <ul class="list-group listOverflow">
-        <li class="list-group-item" v-for="(episode) in episodes" :key="episode.xxx">{{episode.name}}<span
-            class="float-right">{{secondsToTimeString(episode.duration)}}</span></li>
+        <li class="list-group-item" v-for="(episode,i) in episodes" :key="episode.xxx">
+          <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" v-bind:id="i" v-model="episode.checked">
+            <label class="custom-control-label" v-bind:for="i">{{episode.name}}</label>
+            <span class="float-right">{{secondsToTimeString(episode.duration)}}</span>
+          </div>
+        </li>
       </ul>
-      <div v-for="(episode,i) in episodes" :key="episode.xxx">
-        <div class="custom-control custom-checkbox">
-          <input type="checkbox" class="custom-control-input" v-bind:id="i" v-model="episode.checked">
-          <label class="custom-control-label" v-bind:for="i">{{episode.name}}</label>
-        </div>
-      </div>
     </div>
     <button type="button" class="btn btn-primary btn-block" @click="confirm">Confirm</button>
 
@@ -24,17 +23,17 @@
 
   export default {
     name: 'download',
-    data () {
+    data() {
       return {
         episodes: '',
         secondsToTimeString: Split.secondsToTimeString
 
       }
     },
-    mounted () {
+    mounted() {
       this.setChecked()
     },
-    beforeRouteEnter (to, from, next) {
+    beforeRouteEnter(to, from, next) {
       next(vm => {
         if (!Download.get().length) {
           vm.$router.push('landing-page')
@@ -43,7 +42,7 @@
     },
     components: {},
     methods: {
-      confirm () {
+      confirm() {
         let selectedEpisodes = []
         for (const episode of this.episodes) {
           if (episode.checked) {
@@ -53,7 +52,7 @@
         Download.set(selectedEpisodes)
         this.$router.push('videos')
       },
-      setChecked () {
+      setChecked() {
         let episodes = Download.get()
         for (const episode of episodes) {
           episode.checked = true
