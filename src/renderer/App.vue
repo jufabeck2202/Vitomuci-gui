5a5cf28422cf4ef9ff17ceebcd4f43172af98194
jufabeck2202 <ju-fa-beck@t-online.de>
2019-02-19 10:10:57 +0100

    minor

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,26 +1,25 @@
 <template>
   <div id="app">
-    <router-view>
-    </router-view>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'elvue',
-    mounted () {
-      this.$nextTick(() => {
-        this.$el.addEventListener('drop', event => {
-          event.preventDefault()
-        })
-        this.$el.addEventListener('dragover', event => {
-          event.preventDefault()
-        })
-      })
-    }
+export default {
+  name: "elvue",
+  mounted() {
+    this.$nextTick(() => {
+      this.$el.addEventListener("drop", event => {
+        event.preventDefault();
+      });
+      this.$el.addEventListener("dragover", event => {
+        event.preventDefault();
+      });
+    });
   }
+};
 </script>
 
 <style>
-  /* CSS */
+
 </style>
